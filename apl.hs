b15bfda5c845e91389f0f9957abde8442301c43f
Fritz Henglein <henglein@diku.dk>
2013-12-12 18:24:15 +1100

    Compilable Haskell implementations of select APL-like functions
    Some APL notes

--- apl.hs
+++ apl.hs
@@ -2,121 +2,137 @@
 -- Reference implementation (deviates from APL model in several respects, e.g. using 0-indexing)
 -- based on standard Haskell lists
 
+import Data.List (sortBy)
+
 class Elmt a where
   protElmt :: a
 
 type Nat = Int      -- nonnegative integers, for ranks, shapes and indexing; must be >= 0
-type Shape = [Nat]  -- shape of array, must be [], [1] or [d1,...dn] where di > 1
+type Shape = [Nat]  -- shape of array, must be [0] or [d1,...dn] where di > 1 (according to Hains, Mullin '91; different in APL)
 
 toShape :: [Nat] -> Shape
 toShape [] = []
 toShape [1] = [1]
 toShape xs = filter (== 1) xs
 
-type Seq a = [a]    -- content sequence type of arrays
+-- Vectors (rank-1 arrays)
+
+type Vector a = [a] 
+
+vector :: [a] -> Vector a
+vector = id
+
+zilde :: Vector a         -- empty vector
+zilde = []
+
+iota :: Nat -> Vector Nat     -- segment [1..n] as vector
+iota n = [1 .. n]
+
+size :: Vector a -> Nat
+size = length
+
+append :: Vector a -> Vector a -> Vector a
+append = (++)
 
 -- General arrays
 
-data Array a = Array Shape (Seq a) -- array is sequence interpreted by shape
--- For Array s xs: product s = length xs; note: product [] = 1 (neutral element of multipl.)
+data Array a = Array Shape (Vector a) deriving Show
+-- Invariant: For all Array s xs, product s = length xs; Note: product [] = 1 (neutral element of multiplication)
 
+-- Build a rank-1 array from vector 
+array :: Vector a -> Array a
+array vec = Array [size vec] vec
+
+-- shape: lengths of each dimension
 shape :: Array a -> Shape 
 shape (Array s xs) = s
 
+-- underlying vector in array
+ravel :: Array a -> Vector a
+ravel (Array s xs) = xs
+
+-- rank: dimension of array
 rank :: Array a -> Nat   
 rank = length . shape
 
--- auxiliary functions
-contents :: Array a -> Seq a
-contents (Array s xs) = xs     
-
-array :: Shape -> Seq a -> Array a 
-array = Array
-
--- Vectors (rank-1 arrays)
-
-type Vector a = Array a   -- must have rank 1
-
-vector :: Seq a -> Vector a
-vector xs = array [length xs] xs
-
-zilde :: Vector a         -- Empty vector
-zilde = vector []
-
-ravel :: Array a -> Vector a  -- throw away shape
-ravel = vector . contents
-
-iota :: Nat -> Vector Nat   
-iota n = array [n] [1 .. n]
-
-repeat :: Nat -> 
-
 -- Scalars (rank-0 arrays)
 
-type Scalar a = Array a -- must have rank 0
+-- turn value into a scalar (= rank-0) array
+enclose :: a -> Array a 
+enclose x = Array [] [x]
 
-enclose :: a -> Scalar a -- Wraps input into a scalar
-enclose x = array [] [x]
-
-disclose :: Scalar a -> a  -- Unwraps scalar input
+-- project value out of scalar array
+-- precondition: input array must be of rank 0
+disclose :: Array a -> a  
 disclose (Array [] [x]) = x
 disclose (Array _ _) = error "disclose: Input not a scalar"
 
--- 
-
--- APL: mix
-discloseA :: Array (Array a) -> Array a  -- assumes all arrays have equal shape; APL pads 
-discloseA (Arr outer items) 
-  = Arr (outer ++ inner) [ i | Arr _ is <- items, i <- is ] 
+-- splice array scalars into outer array
+-- precondition: all inner arrays have equal shape  (APL pads), at least one inner array
+discloseA :: Array (Array a) -> Array a   
+discloseA (Array outer items) 
+  = Array (outer ++ inner) [ i | Array _ is <- items, i <- is ] 
   where 
-    (Arr inner _ : _) = items
-    
--- APL: split
-chop :: Shape -> [a] -> [Array a]
+    (Array inner _ : _) = items
+
+-- split input sequence into arrays of given shape
+-- precondition: vector size must be multiple of shape product 
+chop :: Shape -> Vector a -> Vector (Array a)
 chop s [] = []
-chop s is = Arr s i : chop s is'
+chop s is = Array s isL : chop s isR
           where
-            (i,is') = splitAt (product s) is
-{-            
-encloseA :: Nat -> Array a -> Array a
-encloseA n (Arr shape items)
-  = Arr outer_shape (chop inner_shape items)
+            (isL, isR) = splitAt (product s) is
+
+encloseA :: Nat             -- number of outer dimensions in result array
+         -> Array a         -- input array
+         -> Array (Array a) -- output array with arrays of remaining dimensions
+-- precondition: n <= rank inputarray
+encloseA n (Array shape items)
+  = Array outer_shape (chop inner_shape items)
   where
     (outer_shape, inner_shape) = splitAt n shape
--}            
+            
+-- n-ary Cartesian product on numbers
+prods :: [[Nat]] -> [[Nat]]
+prods [] = [[]]
+prods (xs : yss) = [ x : ys | x <- xs, ys <- prods yss ]
 
--- APL: dyadic transpose with repeated dimensions yields diagonal
-transpose :: Vector Nat    -- Permutation of (iota (rank arg))
-          -> Array a       -- arg
-          -> Array a
-transpose (Arr [n] perm) (Arr shape items) =
-    -- assert (n == length shape ) $
-    -- Arr (permute perm shape) 
-    --    (scramble ... items)
-    error "transpose: Unimplemented"
+-- Transpose input array based on dimension permutation
+transpose :: Vector Nat    -- Permutation of (iota (rank inputarray))
+          -> Array a       -- input array
+          -> Array a       -- output array
+-- precondition: perm must be permutation of [1..shape arr]
+-- Note: transpose with repeated dimensions yields diagonal in APL
+transpose perm (Array shape items) =
+   let 
+       numberedItems = zip (prods (map iota shape)) items -- positional numbering of items
+       comp :: Nat -> ([Nat], b) -> ([Nat], b) -> Ordering
+       comp n (num1, i1) (num2, i2) = compare (num1 !! (n-1)) (num2 !! (n-1)) -- n-th column comp.
+       sort :: Nat -> [([Nat], b)] -> [([Nat], b)]
+       sort = sortBy . comp
+   in Array (map (\ col -> shape !! (col-1)) perm)  (map snd (foldr sort numberedItems perm))
 
+-- change view of underlying vector
+-- precondition product of new shape must be same as product of old shape
 reshape :: Vector Nat    -- s: the new shape
         -> Array a       -- Arbitrary array    
         -> Array a       -- Shape of result = s
-reshape (Arr [n] s) (Arr s' elts)
-  | null elts = error "Reshape on empty array" -- not necessary if prototypical element is avail
-  | otherwise = array s (take (product s) (cycle elts))
+reshape shapeNew (Array shapeOld elts) = Array shapeNew elts
 
--- APL: each is enclose of map of disclose;
--- e.g. each shape (vector [vector [1, 2, 3], vector [4, 5, 6, 7]])
--- should yield vector [vector [3], vector [4]]
--- Dyadic each
+-- Apply function to each element of array
+-- Note: In APL each is enclose of map of disclose; dyadic each?
 each :: (a -> b) -> Array a -> Array b
-each f (Arr s xs) = Arr s (map f xs)
+each f (Array shape vec) = Array shape (map f vec)
 
-reduce :: (Array a -> Array a -> Array a) 
-          -> Array a   -- All rank one smaller than input
-          -> Array a   -- Input
-          -> Array a   -- Rank one smaller than input
-                       --   except that rank 0 input gives identity
-reduce k z (Arr [] [item])
-  = Arr [] [item]   -- Identity on rank 0
-reduce k z (Arr (s:ss) items)
+-- 
+reduce :: (Array a -> Array a -> Array a) -- reduce function on rank n-1 arrays
+          -> Array a   -- Neutral element
+          -> Array a   -- Input array, rank n, treated as vector (rank 1) of arrays (of rank n-1)
+          -> Array a   -- Reduced array, rank n-1; rank-0 array input gives identity
+-- Acts like identity on scalars (rank-0 arrays)
+-- Precondition: rank arr = n+1 ==> rank z = n, k preserves rank 
+reduce k z (arr @ (Array [] _)) = arr   -- Identity on rank 0
+reduce k z (Array (s:ss) items)
   = foldr k z (chop ss items)
 
 {- to do:
